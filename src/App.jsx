7bc0747879129786f252f110ff1c6fdc91a432aa
Tokinori Shimodaira <66321626+ironikot@users.noreply.github.com>
2022-07-29 11:21:24 +0900

    JSについて。 OnClickなどの中に{}を入れるとJSが書けるので、そこに関数を書いて、 上にまた、関数を書きますと。

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 
 const App = () => {
+  const onClickButton = () => {
+    alert();
+  };
   return (
     <React.Fragment>
       <h1>こんにちは</h1>
       <p1>どうも</p1>
+      <button onClick={onClickButton}>button</button>
     </React.Fragment>
   );
 };
