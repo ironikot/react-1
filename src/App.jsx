3bfe91e4742ea6a54b63aff79d57b50d9b3be1fc
Tokinori Shimodaira <66321626+ironikot@users.noreply.github.com>
2022-07-29 11:14:33 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <h1>こんにちは</h1>
+      <p1>どうも</p1>
+    </React.Fragment>
+  );
+};
+
+export default App;
