39f61c96714073e62e224387380c51ed5a2b348c
Tokinori Shimodaira <66321626+ironikot@users.noreply.github.com>
2022-07-29 11:25:44 +0900

    STYLEの当て方2つ。 {{}}で中に直書きするか、 {}っていう風に書くか

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,14 @@ const App = () => {
   const onClickButton = () => {
     alert();
   };
+  const contentStyle = {
+    color: "blue",
+    fontSize: "18px"
+  };
   return (
     <React.Fragment>
-      <h1>こんにちは</h1>
-      <p1>どうも</p1>
+      <h1 style={{ color: "red" }}>こんにちは</h1>
+      <p1 style={contentStyle}>どうも</p1>
       <button onClick={onClickButton}>button</button>
     </React.Fragment>
   );
